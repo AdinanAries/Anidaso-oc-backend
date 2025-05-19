b084dec1f9fd3983d9bc115d039e1ac71b3f1c19
Dev Moh <adinanaries@outlook.com>
2025-05-19 19:52:19 -0400

    fixing service fee update bug

--- controllers/agent_service_fee_controller.js
+++ controllers/agent_service_fee_controller.js
@@ -20,6 +20,8 @@ const addAgentServiceFee = async (req, res, next) => {
             enabled,
         } = req.body;
 
+        const __id = (req?.body?._id || "");
+        
         let _deleted = (req?.body?.deleted || false);
         was_updated_status="";
 
@@ -37,6 +39,51 @@ const addAgentServiceFee = async (req, res, next) => {
             res.status(400);
             res.send({message: "name, price, and product type fields are required!"});
             return;
+        }
+
+        // Items with Id already exists
+        if(__id){
+
+            // Check if service fee exists 
+            let serviceFeeExists = await AgentServiceFee.findOne({_id: __id});
+
+            if(serviceFeeExists) {
+                res.status(201);
+                let __updated = await AgentServiceFee.updateOne({_id: __id}, {
+                    oc_user_id, 
+                    product, 
+                    name,
+                    price,
+                    enabled,
+                    deleted: _deleted
+                });
+                if (__updated.matchedCount === 0) {
+                    // No document matching the filter was found
+                    console.log("Service fee was not found during update!");
+                    was_updated_status="Service fee was not found during update!";
+                    return
+                } else if (__updated.modifiedCount === 0) {
+                    // A document was matched, but not modified (e.g., the update didn't change any values)
+                    console.log("Service fee already exists however failed on update!");
+                    was_updated_status="Service fee already exists however failed on update!";
+                    return;
+                }else {
+                    console.log("Service fee already exists and was updated!");
+                    was_updated_status="Service fee already exists and was updated!";
+                    // Fetching newly updated record
+                    serviceFeeExists = await AgentServiceFee.findOne({oc_user_id, product, name});
+                }
+                res.send({
+                    _id: serviceFeeExists?._id,
+                    oc_user_id: serviceFeeExists?.oc_user_id,
+                    product: serviceFeeExists?.product,
+                    name: serviceFeeExists?.name,
+                    price: serviceFeeExists?.price,
+                    enabled: serviceFeeExists?.enabled,
+                    was_updated_status: was_updated_status
+                });
+                return;
+            }
         }
 
         // Check if service fee exists 
@@ -63,8 +110,7 @@ const addAgentServiceFee = async (req, res, next) => {
                 console.log("Service fee already exists and was updated!");
                 was_updated_status="Service fee already exists and was updated!";
                 // Fetching newly updated record
-                serviceFeeExists = await AgentServiceFee.findOne({oc_user_id, product, name})
-
+                serviceFeeExists = await AgentServiceFee.findOne({oc_user_id, product, name});
             }
             res.send({
                 _id: serviceFeeExists?._id,
@@ -75,6 +121,7 @@ const addAgentServiceFee = async (req, res, next) => {
                 enabled: serviceFeeExists?.enabled,
                 was_updated_status: was_updated_status
             });
+            return;
         }
 
         // Create new agent service fee
@@ -126,7 +173,7 @@ const getAgentServiceFees = async (req, res, next) => {
             return;
         }
 
-        let service_fees = await AgentServiceFee.find({oc_user_id}).catch(err => {
+        let service_fees = await AgentServiceFee.find({oc_user_id, deleted: false}).catch(err => {
             console.log(err);
             res.status(500);
             res.send({message: "Error while fetching data from DB"});
